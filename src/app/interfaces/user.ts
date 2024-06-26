d5faedb2f8a533a9e93d7205f4c790446ff4f4b5
Volodymyr <wdvlpr@gmail.com>
2024-06-26 16:43:25 +0300

    An interface User is added.

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    login: string;
+    firstName: string;
+    secondName: string;
+    lastName: string;
+    pfone: string;
+    mobile: string;
+    email: string;
+    address: string;
+}
