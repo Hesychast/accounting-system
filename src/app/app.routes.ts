6cf6ea806e5b8e82b08ce6f631a2850d08c675cb
Volodymyr <wdvlpr@gmail.com>
2024-06-26 15:15:26 +0300

    A NotAvailableComponent and a route for it were added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: 'users-management', loadComponent: () => import('./components/module-dashboard/module-dashboard.component').then(m => m.ModuleDashboardComponent) },
     { path: 'users', loadComponent: () => import('./components/users-admin-panel/users-admin-panel.component').then(m => m.UsersAdminPanelComponent) },
-    { path: 'user-edit', loadComponent: () => import('./components/user-editor/user-editor.component').then(m => m.UserEditorComponent) }
+    { path: 'user-edit', loadComponent: () => import('./components/user-editor/user-editor.component').then(m => m.UserEditorComponent) },
+    { path: 'not-available', loadComponent: () => import('./components/not-available/not-available.component').then(m => m.NotAvailableComponent) }
 
 
     // { path: '**', redirectTo: '/404' }
