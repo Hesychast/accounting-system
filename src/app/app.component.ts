634ca442c8786dc784cb79d24824904d9e416992
Volodymyr <wdvlpr@gmail.com>
2024-06-27 19:47:09 +0300

    Fixed redirect to login from the AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule, Router } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
     selector: 'app-root',
@@ -14,11 +14,20 @@ import { lastValueFrom } from 'rxjs';
       RouterModule
     ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'АТ УЗ - Автоматизована система обліку';
   status: boolean = false;
 
-  constructor(public router: Router) {}
+  constructor(
+    public router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.authService.isAdmin()) {
+      this.router.navigate(['/login']);    
+    }
+  }
   
   switchClass() {
     this.status = !this.status;
